fb4cd7bfd7ca01f694787f08d7d8c1e23d3f752a
Abdou TOP <top.abdou100@outlook.fr>
2024-07-13 13:00:48 +0000

    Update main.go

--- main.go
+++ main.go
@@ -8,15 +8,16 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	ip := r.Header.Get("X-Forwarded-For")
-	if ip == "" {
-		ip = "ip"+r.RemoteAddr // Fallback to RemoteAddr if no X-Forwarded-For header is present
-	} else {
-		// X-Forwarded-For peut contenir plusieurs adresses IP séparées par des virgules
-		// Nous prenons la première qui est normalement l'adresse IP du client
-		ip = strings.Split(ip, ",")[0]
-	}
-	fmt.Fprintf(w, "Votre IP publique est : %s", strings.Split(ip, ":")[0])
+	public_ip := r.Header.Get("X-Forwarded-For")
+	ip = r.RemoteAddr
+	// if ip == "" {
+	// 	 // Fallback to RemoteAddr if no X-Forwarded-For header is present
+	// } else {
+	// 	// X-Forwarded-For peut contenir plusieurs adresses IP séparées par des virgules
+	// 	// Nous prenons la première qui est normalement l'adresse IP du client
+	// 	ip = strings.Split(ip, ",")[0]
+	// }
+	fmt.Fprintf(w, "Votre IP publique est : %s ==> %s", strings.Split(ip, ":")[0],strings.Split(public_ip, ":")[0])
 }
 
 func main() {
