1a45ba697517276d5a20e4c1ea1cf2ec887c46b1
Abdou TOP <top.abdou100@outlook.fr>
2024-07-13 12:30:32 +0000

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.RemoteAddr // Fallback to RemoteAddr if no X-Forwarded-For header is present
+	} else {
+		// X-Forwarded-For peut contenir plusieurs adresses IP séparées par des virgules
+		// Nous prenons la première qui est normalement l'adresse IP du client
+		ip = strings.Split(ip, ",")[0]
+	}
+	fmt.Fprintf(w, "Votre IP publique est : %s", strings.Split(ip, ":")[0])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe("192.168.132.129:8080", nil)
+}
