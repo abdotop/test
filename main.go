4ad3160674ad762431f32d33889db71526e25ce6
Abdou TOP <top.abdou100@outlook.fr>
2024-08-14 17:40:55 +0000

    Update main.go

--- main.go
+++ main.go
@@ -3,39 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
-	// "net/http"
 	"os"
-
-	"github.com/abdotop/octopus"
-	// "strings"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	public_ip := r.Header.Get("X-Forwarded-For")
-	ip := r.RemoteAddr
-	// if ip == "" {
-	// 	 // Fallback to RemoteAddr if no X-Forwarded-For header is present
-	// } else {
-	// 	// X-Forwarded-For peut contenir plusieurs adresses IP séparées par des virgules
-	// 	// Nous prenons la première qui est normalement l'adresse IP du client
-	// 	ip = strings.Split(ip, ",")[0]
-	// }
-	fmt.Fprintf(w, "Votre IP publique est : %s ==> %s", ip, public_ip)
-}
-
-// func main() {
-// 	http.HandleFunc("/", handler)
-// 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-// }
-
 func main() {
-	app := octopus.New()
-
-	app.Get("/", func(c *octopus.Ctx) {
-		fmt.Println()
-		ip, err := c.RemoteIP()
-		c.WriteString(fmt.Sprintf("%v \n %v", ip, err))
+	// Define a handler function
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	app.Run(":" + os.Getenv("PORT"))
+	// Start the HTTP server on port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
